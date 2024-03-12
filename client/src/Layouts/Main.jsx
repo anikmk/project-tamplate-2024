dc474b10378253b9cc956b535f2bb3cda7cc636b
anikmk <anikmollik122321@gmail.com>
2024-03-12 15:51:01 +0600

    problem solved

--- client/src/Layouts/Main.jsx
+++ client/src/Layouts/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Componnents/Shared/Footer/Footer";
+import Navbar from "../Componnents/Shared/NavBar/Navbar";
 
 const Main = () => {
   return (
     <div>
       
       <div>
+        <Navbar />
         <Outlet />
+        <Footer />
       </div>
 
     </div>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,14 +1,13 @@
-import toast from "react-hot-toast";
+
 
 
 
 const Home = () => {
-    const handleTost = () => {
-        toast.success('successfully')
-    }
+  
     return (
-        <div>
-           <button onClick={handleTost}>click</button>
+        <div className="py-14">
+         home page
+           
         </div>
     );
 };
